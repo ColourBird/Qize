8872baf40853026838b1d68f5d4beef648ed6122
ColourBird <22011105@c.anabuki-college.net>
2024-07-31 15:36:20 +0900

    new

--- exam/get_answer.php
+++ exam/get_answer.php
@@ -4,9 +4,18 @@ header('Content-Type: application/json');
 
 try {
     $db = new PDO("mysql:dbname=mydb2;host=127.0.0.1;charset=utf8", "root", "");
-    $query = "SELECT * FROM exam";
+    
+    // クエリで問題とユーザーの回答を取得
+    $query = "
+        SELECT exam.*, answer.answer AS userAnswer 
+        FROM exam 
+        LEFT JOIN answer 
+        ON exam.id = answer.exam_id 
+        WHERE answer.no = 1
+        ORDER BY exam.id ASC";
     $stmt = $db->query($query);
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
     echo json_encode($results);
 } catch (PDOException $e) {
     echo json_encode(["error" => "DB接続エラー: " . $e->getMessage()]);
